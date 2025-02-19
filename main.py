2720ac4e9b7879487d56e6d62d9493092e54773b
win-dish <jakob.windisch@student.uibk.ac.at>
2025-02-19 18:29:05 +0100

    initial commit for yolo11 integration

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ import math
 from torchvision import transforms
 import numpy as np
 import os
+import logging
 
 from tqdm import tqdm
+import json
+from ultralytics import YOLO
 
 from utils.datasets import letterbox
 from utils.general import non_max_suppression_kpt
@@ -15,26 +18,29 @@ from utils.plots import output_to_keypoint, plot_skeleton_kpts
 
 def fall_detection(poses):
     for pose in poses:
-        xmin, ymin = (pose[2] - pose[4] / 2), (pose[3] - pose[5] / 2)
-        xmax, ymax = (pose[2] + pose[4] / 2), (pose[3] + pose[5] / 2)
-        left_shoulder_y = pose[23]
-        left_shoulder_x = pose[22]
-        right_shoulder_y = pose[26]
-        left_body_y = pose[41]
-        left_body_x = pose[40]
-        right_body_y = pose[44]
-        len_factor = math.sqrt(((left_shoulder_y - left_body_y) ** 2 + (left_shoulder_x - left_body_x) ** 2))
-        left_foot_y = pose[53]
-        right_foot_y = pose[56]
-        dx = int(xmax) - int(xmin)
-        dy = int(ymax) - int(ymin)
-        difference = dy - dx
-        if left_shoulder_y > left_foot_y - len_factor and left_body_y > left_foot_y - (
-                len_factor / 2) and left_shoulder_y > left_body_y - (len_factor / 2) or (
-                right_shoulder_y > right_foot_y - len_factor and right_body_y > right_foot_y - (
-                len_factor / 2) and right_shoulder_y > right_body_y - (len_factor / 2)) \
-                or difference < 0:
-            return True, (xmin, ymin, xmax, ymax)
+        if pose.boxes and pose.keypoints:
+            xmin = pose.boxes.xyxy[0][0]
+            xmax = pose.boxes.xyxy[0][2]
+            ymin = pose.boxes.xyxy[0][1]
+            ymax = pose.boxes.xyxy[0][3]
+            left_shoulder_y = pose.keypoints.xy[0][5][1]
+            left_shoulder_x = pose.keypoints.xy[0][5][0]
+            right_shoulder_y = pose.keypoints.xy[0][6][1]
+            left_body_y = pose.keypoints.xy.data[0][11][1]
+            left_body_x = pose.keypoints.xy.data[0][11][0]
+            right_body_y = pose.keypoints.xy.data[0][12][1]
+            len_factor = math.sqrt(((left_shoulder_y - left_body_y) ** 2 + (left_shoulder_x - left_body_x) ** 2))
+            left_foot_y = pose.keypoints.xy.data[0][15][1]
+            right_foot_y = pose.keypoints.xy.data[0][16][1]
+            dx = pose.boxes.xywh[0][2]
+            dy = pose.boxes.xywh[0][3]
+            difference = dy - dx
+            if left_shoulder_y > left_foot_y - len_factor and left_body_y > left_foot_y - (
+                    len_factor / 2) and left_shoulder_y > left_body_y - (len_factor / 2) or (
+                    right_shoulder_y > right_foot_y - len_factor and right_body_y > right_foot_y - (
+                    len_factor / 2) and right_shoulder_y > right_body_y - (len_factor / 2)) \
+                    or difference < 0:
+                return True, (xmin, ymin, xmax, ymax)
     return False, None
 
 
@@ -47,12 +53,10 @@ def falling_alarm(image, bbox):
 
 def get_pose_model():
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    print("device: ", device)
-    weigths = torch.load('yolov7-w6-pose.pt', map_location=device, weights_only=False)
-    model = weigths['model']
-    _ = model.float().eval()
-    if torch.cuda.is_available():
-        model = model.half().to(device)
+    #print("device: ", device)
+    model = YOLO('yolo11l-pose.pt')
+    #if torch.cuda.is_available():
+    #    model = model.half().to(device)
     return model, device
 
 
@@ -63,10 +67,10 @@ def get_pose(image, model, device):
     if torch.cuda.is_available():
         image = image.half().to(device)
     with torch.no_grad():
-        output, _ = model(image)
-    output = non_max_suppression_kpt(output, 0.25, 0.65, nc=model.yaml['nc'], nkpt=model.yaml['nkpt'],
-                                     kpt_label=True)
-    with torch.no_grad():
+        output = model(image)
+    #output = non_max_suppression_kpt(output, 0.25, 0.65, nc=model.yaml['nc'], nkpt=model.yaml['kpt_shape'][0],
+    #                                 kpt_label=True)
+    #with torch.no_grad():
         output = output_to_keypoint(output)
     return image, output
 
@@ -85,8 +89,29 @@ def prepare_vid_out(video_path, vid_cap):
     out = cv2.VideoWriter(out_video_name, cv2.VideoWriter_fourcc(*'mp4v'), 30, (resize_width, resize_height))
     return out
 
+def write_data_to_json(fall_start, fall_end, video, file):
+    event_info = f"Fall event detected from {fall_start:.2f} sec to {fall_end:.2f} sec"
+
+    # Load existing data or initialize empty dict if file doesn't exist
+    try:
+        with open(file, "r") as f:
+            data = json.load(f)
+    except (FileNotFoundError, json.JSONDecodeError):
+        data = {}
+
+    # Add or update entry with filename as key
+    data[video] = event_info
+
+    # Write updated data back to the JSON file
+    with open(file, "w") as f:
+        json.dump(data, f, indent=4)
+
 
 def process_video(video_path):
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s [%(levelname)s]: %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    logger = logging.getLogger(__name__)
     vid_cap = cv2.VideoCapture(video_path)
 
     if not vid_cap.isOpened():
@@ -114,6 +139,7 @@ def process_video(video_path):
     fall_event_end = 0
     fall_events = []  # List to store (start, end) times for fall events
 
+    logger.info("processing video %s", video_path)
     # Process each frame with its index (to compute timestamp)
     for frame_idx, image in enumerate(tqdm(_frames)):
         current_time = frame_idx / fps  # Timestamp in seconds
@@ -134,7 +160,7 @@ def process_video(video_path):
                 duration = fall_event_end - fall_event_start
                 if duration >= 0.2:
                     fall_events.append((fall_event_start, fall_event_end))
-                    print(f"Fall event detected from {fall_event_start:.2f} sec to {fall_event_end:.2f} sec")
+                    write_data_to_json(fall_start=fall_event_start, fall_end=fall_event_end, video = video_path, file="detected_falls.json")
                 # Reset the fall event flag
                 fall_event_active = False
 
@@ -145,7 +171,7 @@ def process_video(video_path):
         duration = fall_event_end - fall_event_start
         if duration >= 0.2:
             fall_events.append((fall_event_start, fall_event_end))
-            print(f"Fall event detected from {fall_event_start:.2f} sec to {fall_event_end:.2f} sec")
+            write_data_to_json(fall_start=fall_event_start, fall_end=fall_event_end, video = video_path, file="detected_falls.json")
 
     vid_out.release()
     vid_cap.release()
@@ -153,7 +179,7 @@ def process_video(video_path):
 
 
 if __name__ == '__main__':
-    videos_path = 'fall_dataset/videos'
+    videos_path = 'fall_dataset/custom'
     for video in os.listdir(videos_path):
         video_path = os.path.join(videos_path, video)
         process_video(video_path)
